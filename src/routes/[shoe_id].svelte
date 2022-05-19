7a0233f048bdef5d064ebcb63900e2ef87485364
Rick Zolnierek <rzolnier@stud.hs-heilbronn.de>
2022-05-19 14:03:58 +0200

    added missing file to git

---
+++ src/routes/[shoe_id].svelte
@@ -0,0 +1,19 @@
+<script context="module" lang="ts">       
+</script>
+
+<script lang="ts">
+	export let shoe;
+</script>
+
+<main>
+	<img alt="running shoe" />
+	<h4>{shoe.name}</h4>
+	<h4>{shoe.price}</h4>
+	<form>
+		<select> shoeSize </select>
+	</form>
+	>
+	<button>Add to Cart</button>
+
+	<p>{shoe.description}</p>
+</main>
